798a11e53d3269ce066fa022694e342c8a25c068
Yash Rank <46014872+YASHRANK@users.noreply.github.com>
2020-08-12 17:01:25 +0530

    Update settings.py

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$)8)f53!%&s@(1jn0rh9cz_!1+dpjkf#6hm%fvr3bjf-dj-b+7'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
